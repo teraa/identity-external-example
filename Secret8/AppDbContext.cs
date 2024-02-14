3ff4c65daa3e7c3f2febcddae315fff30b26ab1e
Tera <24725862+teraa@users.noreply.github.com>
2024-02-14 20:44:33 +0100

    create extension method

--- Secret8/AppDbContext.cs
+++ Secret8/AppDbContext.cs
@@ -8,7 +8,7 @@ public sealed class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
-    
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
@@ -19,27 +19,68 @@ public sealed class AppDbContext : IdentityDbContext<AppUser>
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        modelBuilder.UseSnakeCaseIdentityNames<AppUser>();
+    }
+}
+
+public static class ModelBuilderExtensions
+{
+    public static ModelBuilder UseSnakeCaseIdentityNames<TUser>(this ModelBuilder modelBuilder)
+        where TUser : IdentityUser<string>
+        => UseSnakeCaseIdentityNames
+        <
+            string,
+            TUser,
+            IdentityUserClaim<string>,
+            IdentityUserLogin<string>,
+            IdentityUserRole<string>,
+            IdentityUserToken<string>,
+            IdentityRole,
+            IdentityRoleClaim<string>
+        >(modelBuilder);
+
+    public static ModelBuilder UseSnakeCaseIdentityNames<
+        TKey,
+        TUser,
+        TUserClaim,
+        TUserLogin,
+        TUserRole,
+        TUserToken,
+        TRole,
+        TRoleClaim>(this ModelBuilder modelBuilder)
+        where TKey : IEquatable<TKey>
+        where TUser : IdentityUser<TKey>
+        where TUserClaim : IdentityUserClaim<TKey>
+        where TUserLogin : IdentityUserLogin<TKey>
+        where TUserRole : IdentityUserRole<TKey>
+        where TUserToken : IdentityUserToken<TKey>
+        where TRole : IdentityRole<TKey>
+        where TRoleClaim : IdentityRoleClaim<TKey>
+    {
+        // https://github.com/efcore/EFCore.NamingConventions/issues/2#issuecomment-612651161
         
-        modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("asp_net_role_claims");
-        
-        modelBuilder.Entity<IdentityRole>(b =>
+        modelBuilder.Entity<TRoleClaim>().ToTable("asp_net_role_claims");
+
+        modelBuilder.Entity<TRole>(b =>
         {
             b.ToTable("asp_net_roles");
             b.HasIndex(e => e.NormalizedName).HasDatabaseName("ix_asp_net_roles_normalized_name");
         });
-        
-        modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("asp_net_user_claims");
-        modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("asp_net_user_logins");
-        modelBuilder.Entity<IdentityUserRole<string>>().ToTable("asp_net_user_roles");
-        
-        modelBuilder.Entity<AppUser>(b =>
+
+        modelBuilder.Entity<TUserClaim>().ToTable("asp_net_user_claims");
+        modelBuilder.Entity<TUserLogin>().ToTable("asp_net_user_logins");
+        modelBuilder.Entity<TUserRole>().ToTable("asp_net_user_roles");
+
+        modelBuilder.Entity<TUser>(b =>
         {
             b.ToTable("asp_net_users");
             b.HasIndex(e => e.NormalizedUserName).HasDatabaseName("ix_asp_net_users_normalized_user_name");
             b.HasIndex(e => e.NormalizedEmail).HasDatabaseName("ix_asp_net_users_normalized_email");
         });
-        
-        modelBuilder.Entity<IdentityUserToken<string>>().ToTable("asp_net_user_tokens");
+
+        modelBuilder.Entity<TUserToken>().ToTable("asp_net_user_tokens");
+
+        return modelBuilder;
     }
 }
 
