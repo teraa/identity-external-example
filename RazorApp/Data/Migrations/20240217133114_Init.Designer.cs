49511087b2ad457b0f8b2adcfd603168118db375
Tera <24725862+teraa@users.noreply.github.com>
2024-02-17 14:32:13 +0100

    add migration

--- RazorApp/Data/Migrations/20240217133114_Init.Designer.cs
+++ RazorApp/Data/Migrations/20240217133114_Init.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorApp.Data;
 
@@ -10,9 +11,11 @@ using RazorApp.Data;
 namespace RazorApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240217133114_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
